af1e19a8f6a49ca49abec91d0bcbf08a9801c8c7
nakul009 <sahayatanakul2867@gmail.com>
2018-09-09 10:29:54 -0700

    Slice Function use in Python

---
+++ sliceFunction.py
@@ -0,0 +1,13 @@
+'#We can create any slice of string from whatever we want #'
+anyVar = "nakul is here yes I am here"
+print(anyVar[0])
+print("\n")
+print(anyVar[9])
+print("\n")
+print(anyVar[0:5])
+print("\n")
+print("This is my name "+anyVar[0:5])
+print("\n")
+'# Anything After 2#'
+print(anyVar[2:])
+print("\n")
