d749dabd49c1c610d51dcd3548ebb21b0eb72afb
nakul009 <sahayatanakul2867@gmail.com>
2018-09-09 12:40:45 -0700

    Tuples in Python

---
+++ tuplesPython.py
@@ -0,0 +1,11 @@
+"#tuples are immutable  we cannot change its values as we can in List #"
+tuple1 = ("CA", 45, "SMDM", 55, "DAA")
+"# It gives error tuple.append('hII') #"
+print(tuple1)
+tuple2 = ("DAA", 444, "SMDM", "ANDROID", 44)
+print(tuple2)
+"# Tuples has slicing values #"
+print(tuple2[0])
+print(tuple2[0:3])
+"# delete tuple full"
+" # del tuple2 print(tuple2) It will give us errors #"
