35fa81d6de70f59b4eb9dc7f801872bcd1dbb123
nakul009 <sahayatanakul2867@gmail.com>
2018-09-09 18:33:03 -0700

    Exception in Python

---
+++ exceptionInPython.py
@@ -0,0 +1,10 @@
+try:
+    if(a > 9):
+        print("Hii")
+except:
+    print("I am in Exception")
+try:
+    if(a > 9):
+        print("Hii")
+except Exception as e:
+    print("I am in Exception")
