3dd9845ad1f1370a7be4a1801adc8b04b4288cb4
nakul009 <sahayatanakul2867@gmail.com>
2018-09-09 19:38:24 -0700

    Functions and Some basic functions in Python

---
+++ functionsInPython.py
@@ -0,0 +1,39 @@
+# Functions in python #
+def sumOfValues(var1, var2):
+    var3 = var1+var2
+    return var3
+
+
+x = sumOfValues(12, 10)
+print(x)
+
+# Built In Functions in Python #
+# Absolute Value of Number #
+print(abs(-90.77))
+
+# Bool Function #
+print(bool(0))
+print(bool(1))
+print(bool(7 > 8))
+print(bool(None))
+
+# dir Function gives all the functions that are available of datatype put into dir brackets#
+print(dir(["asde"]))
+x = "Hii"
+print(dir(x))
+
+# help function in Python #
+print(help(x.upper))
+
+# evaluate function exec function is for complex code eval is for simple code#
+print(eval("9*4"))
+print(eval("print('I am here')"))
+
+# Built in Functions in Python #
+x = "13"
+if(int(x) > 2):
+    print("Hi")
+else:
+    print("No")
+print(float(x))
+print(str(14))
